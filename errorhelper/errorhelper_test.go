3232d6c27fff89e281ce027d9524fed847cd9fe0
Bhaskar Brahma <bhbrahma@microsoft.com>
2019-04-26 14:46:59 -0700

    removed commented code added tests for error with stack for panic scenarios

--- errorhelper/errorhelper_test.go
+++ errorhelper/errorhelper_test.go
@@ -21,13 +21,23 @@ func ErrorWithStack(stackDepth int, err error) error {
 	}
 }
 
+func ErrorWithoutStack(stackDepth int, err error) error {
+	if stackDepth == 0 {
+		if err != nil {
+			return err
+		} else {
+			return fmt.Errorf("reached the bottom of the error call stack")
+		}
+	} else {
+		return ErrorWithoutStack(stackDepth-1, err)
+	}
+}
+
 func TestErrorsWrap(t *testing.T) {
 	err1 := ErrorWithStack(5, fmt.Errorf("misc error"))
-	//err2 := ErrorWithWrap(5, fmt.Errorf("misc error"))
 	err2 := ErrorWithStack(5, nil)
 
 	t.Logf("the error stack trace is: %+v", err1)
-	//t.Logf("the error stack trace is %s, %+v", err2, err2)
 	t.Logf("the error stack trace is: %+v", err2)
 
 	if !strings.Contains(err1.Error(), "ErrorWithStack") {
@@ -45,4 +55,24 @@ func TestErrorsWrap(t *testing.T) {
 	if !strings.Contains(err2.Error(), "Reached the bottom of the error call stack") {
 		t.Fatal("error doesn't base error")
 	}
+}
+
+func TestPanicWithCallStack(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Logf("Recovered %+v", r)
+		}
+	}()
+	err := ErrorWithStack(5, fmt.Errorf("misc error"))
+	panic(err)
+}
+
+func TestPanicWithoutCallStack(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Logf("Recovered %+v", r)
+		}
+	}()
+	err := ErrorWithoutStack(5, fmt.Errorf("misc error"))
+	panic(err)
 }
